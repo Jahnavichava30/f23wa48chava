ae02570e5297c6159a3c5e66136cdafc139dbd29
Jahnavichava30 <142965078+Jahnavichava30@users.noreply.github.com>
2023-10-25 15:47:54 -0500

    update random  value

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+let sum=0;
+let more=0;
+
+router.get('/computation', function(req, res, next){
+  var x = req.x;
+  if(!x){
+    x = Math.random()*260;
+  }
+  res.send(`Math.floor(${x}) is:${Math.floor(x)}`);
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more++;
+  sum += more;
+  res.send(`Sum is: ${sum}`);
 });
 
 module.exports = router;
